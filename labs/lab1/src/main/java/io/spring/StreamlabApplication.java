4a0ae5e7f84bc7fed67114b9a123864336daac15
Sabby Anandan <sabby.anandan@gmail.com>
2017-02-18 10:55:15 -0800

    Reset lab exercise back to being a template as opposed to a running sample
    - comment business logic
    - comment dependencies
    - format lab instructions

--- labs/lab1/src/main/java/io/spring/StreamlabApplication.java
+++ labs/lab1/src/main/java/io/spring/StreamlabApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.integration.annotation.ServiceActivator;
 
-@EnableBinding(Processor.class)
 @SpringBootApplication
 public class StreamlabApplication {
 
@@ -14,10 +13,10 @@ public class StreamlabApplication {
         SpringApplication.run(StreamlabApplication.class, args);
     }
 
-    @ServiceActivator(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+    /*@ServiceActivator(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
     public Object uppercase(Object incomingPayload) {
         String outgoingPayload = incomingPayload.toString().toUpperCase();
         System.out.println("Incoming payload=[" + incomingPayload + "]" + " Outgoing payload=[" + outgoingPayload + "]");
         return outgoingPayload;
-    }
+    }*/
 }
